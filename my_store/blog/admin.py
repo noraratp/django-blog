7d34011e6a5e288f86f944de58e3c629931da473
pich <pnoraratp@gmail.com>
2019-06-11 01:06:12 +0700

    Add new model in admin

--- my_store/blog/admin.py
+++ my_store/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .dashboard.models import Post, Category
+from .dashboard.models import Post, Category, PostCategory
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(PostCategory)
