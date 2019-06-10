71a30cb87cdc83841e9516cc5da9b154cc245c9a
pich <pnoraratp@gmail.com>
2019-06-11 01:07:31 +0700

    Remove space

--- my_store/blog/dashboard/forms.py
+++ my_store/blog/dashboard/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 
 class PostSearchForm(forms.Form):
-  
     title = forms.CharField(
         max_length=255, required=False, label=('Post title'))
     author = forms.CharField(max_length=16, required=False, label=('Author'))
@@ -11,4 +10,5 @@ class PostSearchForm(forms.Form):
         cleaned_data = super().clean()
         cleaned_data['title'] = cleaned_data['title'].strip()
         cleaned_data['author'] = cleaned_data['author'].strip()
+
         return cleaned_data
