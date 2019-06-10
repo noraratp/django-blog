c730d979c513c30c535d13c99fbb0371e9285936
pich <pnoraratp@gmail.com>
2019-06-11 01:08:09 +0700

    Change attribute aurthor FK Model User

--- my_store/blog/dashboard/models.py
+++ my_store/blog/dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from oscar.core.compat import AUTH_USER_MODEL
 
 
 class Category(models.Model):
@@ -15,15 +16,24 @@ class Post(models.Model):
     featured_image = models.ImageField(
         ("Featured Image"), upload_to=settings.OSCAR_IMAGE_FOLDER, max_length=255)
     post_date = models.DateTimeField('date published')
-    author = models.CharField(max_length=200)
-    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name=("Author"))
+    category = models.ManyToManyField(Category, through='PostCategory')
     excerpt = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-post_date']
 
     def __str__(self):
         return self.title
 
 
-# class ProductFilter(django_filters.FilterSet):
-#     class Meta:
-#         model = Post
-#         fields = ['author', 'title']
+class PostCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}-{}".format(self.category, self.post.title)
