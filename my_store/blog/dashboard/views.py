97b16a047f469c72bd2cb3b53c0a282617992e51
pich <pnoraratp@gmail.com>
2019-07-02 12:06:19 +0700

    Add Create View and Detail View

--- my_store/blog/dashboard/views.py
+++ my_store/blog/dashboard/views.py
@@ -1,7 +1,16 @@
+from django.contrib import messages
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django_tables2 import SingleTableView
+from django.views import generic
+from django.urls import reverse
+
+
+from .forms import PostSearchForm, PostForm
+from .formsets import CategoryGroupFormSet
 from .models import Post
 from .tables import PostTable
-from .forms import PostSearchForm
-from django_tables2 import SingleTableView
 
 
 class BlogListView(SingleTableView):
@@ -36,3 +45,67 @@ class BlogListView(SingleTableView):
             queryset = queryset.filter(title__icontains=data['title'])
 
         return queryset
+
+
+class BlogCreateView(generic.CreateView):
+
+    template_name = 'dashboard/blog/create.html'
+    form_class = PostForm
+    model = Post
+    category_formset_class = CategoryGroupFormSet
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['category_formset'] = self.category_formset_class(instance=self.object)
+        return ctx
+
+    def process_all_forms(self, form):
+        if form.is_valid():
+            self.object = form.save(commit=False)
+        formset = self.category_formset_class(self.request.POST, instance=self.object)
+        is_valid = form.is_valid() and formset.is_valid()
+        if is_valid:
+            return self.forms_valid(form, formset)
+        else:
+            return self.forms_invalid(form, formset)
+
+    form_valid = form_invalid = process_all_forms
+
+    def forms_valid(self, form, formset):
+        self.object = form.save()
+        formset.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def forms_invalid(self, form, formset):
+        messages.error(self.request,
+                       ("Your submitted data was not valid - please "
+                        "correct the errors below"))
+        context = self.get_context_data(form=form)
+        context['category_formset'] = formset
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        messages.success(self.request, ('Post created successfully'))
+
+        action = self.request.POST.get('action')
+        if action == 'continue':
+            # return reverse('blog-dashboard:blog-post-detail-update', kwargs={'id': self.object.id})
+            return reverse('blog:blog_list_view')
+        else:
+            return reverse('blog:blog_list_view')
+
+
+class BlogDetailView(generic.DetailView):
+    template_name = 'dashboard/blog/detail.html'
+    form_class = PostForm
+    context_object_name = 'post_detail'
+    category_formset_class = CategoryGroupFormSet
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['form'] = self.form_class()
+        ctx['category_formset'] = self.category_formset_class(instance=self.object)
+        return ctx
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Post, pk=self.kwargs['id'])
