99a684d3862f524dc13ecc9348ad1a91de6ca93a
pich <pnoraratp@gmail.com>
2019-07-02 12:05:46 +0700

    Add URL Create And Detail

--- my_store/blog/dashboard/apps.py
+++ my_store/blog/dashboard/apps.py
@@ -1,5 +1,5 @@
 from oscar.core.application import Application
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from django.conf.urls import url
 
 
@@ -10,11 +10,21 @@ class BlogApplication(Application):
 
     default_permissions = ['is_staff', ]
     blog_list_view = BlogListView
+    blog_detail_view = BlogDetailView
+    blog_create_view = BlogCreateView
 
     def get_urls(self):
         urls = [
             url(r'^blog_list_view/$', self.blog_list_view.as_view(),
-                name='blog_list_view')
+                name='blog_list_view'),
+            url(r'^blog_list_view/post/create/$',
+                self.blog_create_view.as_view(),
+                name='blog_create_view'),
+            url(r'^blog_list_view/(?P<pk>\d+)/$',
+                self.blog_detail_view.as_view(),
+                name='blog_detail_view'),
+            
+            
         ]
         return self.post_process_urls(urls)
 
