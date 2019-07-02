a86ab64c5cdc2808d11cf903305ea5c5f569c232
pich <pnoraratp@gmail.com>
2019-07-02 12:06:03 +0700

    Add table action

--- my_store/blog/dashboard/tables.py
+++ my_store/blog/dashboard/tables.py
@@ -6,6 +6,16 @@ class PostTable(tables.Table):
 
     author = tables.TemplateColumn('{{ record.author.email | default:"-" }}')
 
+    title = tables.TemplateColumn(
+        verbose_name=('Title'),
+        template_name='dashboard/blog/blog_row_title.html',
+        order_by='title', accessor=('title'))
+
+    actions = tables.TemplateColumn(
+        verbose_name=('Actions'),
+        template_name='dashboard/blog/blog_row_actions.html',
+        orderable=False)
+
     class Meta:
         model = Post
         template_name = 'django_tables2/bootstrap.html'
